b4091cab544faf5f8461745beb7938e7f1fa83f9
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-04-27 01:05:31 +0000

    updated index, still need to complete results and style

--- index.php
+++ index.php
@@ -12,10 +12,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon[1].png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32[1].png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16[1].png">
-    <link rel="manifest" href="./favicon/site[2].webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
+    <link rel="manifest" href="./favicon/site.webmanifest">
       
     <!-- Link to CSS file -->
     <link rel="stylesheet" href="./css/style.css">
@@ -31,17 +31,81 @@
       echo "<h3>This program takes in online orders, which includes 3 different sizes, a variety of toppings as protein, and 0-2 drinks.</h3><h6>*Toppings and salad dressing are optional.</h6>";
     ?>
     <center>
-    <img src="./images/popcorn_movie.jpg" alt="Popcorn with Movie Image" width="10%" height="10%">
+    <img src="./images/green_salad.jpeg" alt="Green Salad Image" width="10%" height="10%">
       
-    <!-- Form for the user's age -->
-    <form action="./results.php" method="post" target="results">
-      <label for="lblUserAge">Age:</label>
-      <input type="number" id="user-age" step="1" placeholder="Eg. 5" name="user-age"><br><br>
-      <input type="submit" value="Submit Age">
-    </form>
+    <!-- Table with menu items and their prices -->
+      <h3>Menu and Prices:</h3>
+        <center>
+      <table>
+        <tr>
+          <th class="border">Item</th>
+          <th class="border">Price</th>
+        </tr>
+        <tr>
+          <td class="border">Green Salad</td>
+          <td class="border">Small: $5.99<br>Medium: $7.99<br>Large: $10.99</td>
+        </tr>
+        <tr>
+          <td class="border">Toppings</td>
+          <td class="border">$1.50 each</td>
+        </tr>
+        <tr>
+          <td class="border">Drinks</td>
+          <td class="border">$2.50 each</td>
+        </tr>
+        <tr>
+          <td class="border">Salad Dressing</td>
+          <td class="border">$0.50</td>
+        </tr>
+      </table>
+        </center>
       
-    <!-- iframe for the response to show on the web page. -->
-    <iframe id="results" name="results"></iframe>
-    </center>
+      <!-- form for online order (user input) -->
+      <form action="/results.php">
+        <!-- Drop-Down Menu for size of salad -->
+        <label for="salad-size"><h5>Size of salad:</h5></label>
+        <select id="salad-size" name="salad-size">
+          <option value="0">-- Size --</option>
+          <option value="small">Small</option>
+          <option value="medium">Medium</option>
+          <option value="large">Large</option>
+        </select><br><br>
+        <h5>Toppings:</h5>
+      </center>
+        <!-- checkboxes for toppings -->
+        <input type="checkbox" id="gr-chicken" name="gr-chicken" value="gr-chicken">
+        <label for="gr-chicken"> Grilled Chicken Strips</label><br>
+        <input type="checkbox" id="s-steak" name="s-steak" value="s-steak">
+        <label for="s-steak"> Stirloin Steak Strips</label><br>
+        <input type="checkbox" id="b-beans" name="b-beans" value="b-beans">
+        <label for="b-beans"> Black Beans</label><br>
+        <input type="checkbox" id="tuna" name="tuna" value="tuna">
+        <label for="tuna"> Tuna</label><br>
+        <input type="checkbox" id="peanuts" name="peanuts" value="peanut">
+        <label for="peanuts"> Peanuts</label>
+        <br><br>
+        <center>
+        <!-- Drop-Down Menu for Number of Drinks -->
+        <label for="drinks"><h5>Number of Drinks:</h5></label>
+        <select id="drinks" name="drinks">
+          <option value="0">-- # of Drinks --</option>
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+        </select><br><br>
+        <!-- Radio Button for Salad Dressing -->
+        <h5>Salad Dressing?</h5>
+        <input type="radio" id="yes" name="dressing-options" value="yes">
+        <label for="yes">Yes</label><br>
+        <input type="radio" id="no" name="dressing-options" value="no">
+        <label for="no">No</label><br>
+        <br><br>
+        <!-- SUBMIT BUTTON -->
+        <input type="submit" id="button" value="Submit Order!">
+          <br>
+      </form>
+      <!-- iframe for the response to show on the web page. -->
+      <iframe id="results" name="results"></iframe>
+        </center>
   </body>
 </html>
