550c2b8d616f7d89b5bde7a26b73b4ea5ddea95b
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-05-02 19:54:50 +0000

    linter error fixing

--- results.php
+++ results.php
@@ -9,7 +9,7 @@
   define("PRICE_DRINKS", 2.50);
   define("PRICE_DRESSING", 0.50);
 
-  //initializing size and salad dressing cost for later if... else if...else statement 
+  //initializing size and salad dressing cost for later if... else if...else statement
   $sizeCost = 0;
   $dressingCost = 0;
 
@@ -22,7 +22,7 @@
   $nOfToppings = 0;
 
   //Changing the number of toppings based on the number of checkboxes checked
-    if (!empty($_POST["checkboxes"])) {
+if (!empty($_POST["checkboxes"])) {
     $nOfToppings = count($_POST["checkboxes"]);
 }
 
@@ -31,32 +31,32 @@
 
   //determining the cost of the size using a if...else if...else statement
   //cost for small size
-  if ($size == "small") {
+if ($size == "small") {
     $sizeCost = PRICE_SMALL;
-  }
+}
   //cost for medium size
-  elseif ($size == "medium") {
+elseif ($size == "medium") {
     $sizeCost = PRICE_MEDIUM;
-  }
+}
   //cost for large size
-  elseif ($size == "large") {
+elseif ($size == "large") {
     $sizeCost = PRICE_LARGE;
-  }
+}
   //else user did not put a size
-  else {
+else {
     echo "Please enter a size.";
     exit();
-  }
+}
 
   //determining the cost of drinks
   $drinksCost = $nOfDrinks * PRICE_DRINKS;
   //to check if the user wanted salad dressing, adds 0.50 to the subtotal
-  if ($userDressing == "yes") {
+if ($userDressing == "yes") {
     $dressingCost = PRICE_DRESSING;
-  }
-  else {
+}
+else {
     $dressingCost = 0;
-  }
+}
 
   //calculating the totals
   $subtotal = $sizeCost + $toppingsCost + $drinksCost + $dressingCost;
@@ -65,4 +65,3 @@
 
   //displaying the cost (totals) to the screen
   echo "Your subtotal is $" . number_format($subtotal, 2) . ".<br>The amount of HST added is $" . number_format($tax, 2) . ".<br>Your total is $" . number_format($total, 2) . ".";
-?>
